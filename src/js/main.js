a2439ce58896a55b2f2d1a4c7f0bc26e3cbb5dcd
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-03-16 17:15:11 +0200

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,33 @@
+let btn = document.querySelector("button");
+let html_icon = document.querySelector(".html");
+let css_icon = document.querySelector(".css");
+let js_icon = document.querySelector(".js");
+let bs_icon = document.querySelector(".bs");
+let sass_icon = document.querySelector(".sass");
+let react_icon = document.querySelector(".react");
+btn.onclick = ()=>{
+    window.open("https://www.linkedin.com/in/hamdy-abdal-majeed-b67b32246/overlay/background-image/")    
+};
+
+
+html_icon.onclick = ()=>{
+    window.open("https://developer.mozilla.org/en-US/docs/Web/HTML")    
+};
+css_icon.onclick = ()=>{
+    window.open("https://developer.mozilla.org/en-US/docs/Web/CSS")    
+};
+js_icon.onclick = ()=>{
+    window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript")    
+};
+js_icon.onclick = ()=>{
+    window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript")    
+};
+bs_icon.onclick = ()=>{
+    window.open("https://getbootstrap.com")
+}
+sass_icon.onclick = ()=>{
+    window.open("https://sass-lang.com")
+}
+react_icon.onclick = ()=>{
+    window.open("https://reactjs.org")
+}

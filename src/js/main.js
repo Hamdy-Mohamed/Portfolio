6e4ec45e2e714e92951e6d317ef7a6df907dbee7
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-03-17 20:44:09 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector("button");
+let btn = document.querySelector(".animate");
 let html_icon = document.querySelector(".html");
 let css_icon = document.querySelector(".css");
 let js_icon = document.querySelector(".js");
@@ -29,4 +29,31 @@ sass_icon.onclick = ()=>{
 react_icon.onclick = ()=>{
     window.open("https://reactjs.org")
 }
+
+alert("if you want to change background double click in any space😄 😄")
+
+let curoser = document.querySelector(".curoser");
+let curoser_lazy = document.querySelector(".curoser-lazy");
+let editCuroser = function editCuroser(event){
+    curoser.style.left = event.clientX + 'px';
+    curoser.style.top = event.clientY + 'px';
+    curoser_lazy.style.left = event.clientX + 'px';
+    curoser_lazy.style.top = event.clientY + 'px';
+}
+
+window.addEventListener('mousemove', editCuroser)
+
+
+
+let media = document.querySelector(".container")
+let change = document.addEventListener('dblclick', function(event){
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+  
+    let red = mouseX % 256;
+    let green = mouseY % 256;
+    let blue = (mouseX + mouseY) % 256;
+  
+    document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+});
 

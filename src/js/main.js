9b64b79ed9f242314bf58bd34bee984f183474ce
Hamdy-Mohamed <121273938+Hamdy-Mohamed@users.noreply.github.com>
2023-03-19 18:41:36 +0200

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -5,55 +5,73 @@ let js_icon = document.querySelector(".js");
 let bs_icon = document.querySelector(".bs");
 let sass_icon = document.querySelector(".sass");
 let react_icon = document.querySelector(".react");
-btn.onclick = ()=>{
-    window.open("https://www.linkedin.com/in/hamdy-abdal-majeed-b67b32246/overlay/background-image/")    
+btn.onclick = () => {
+  window.open(
+    "https://www.linkedin.com/in/hamdy-abdal-majeed-b67b32246/overlay/background-image/"
+  );
 };
-html_icon.onclick = ()=>{
-    window.open("https://developer.mozilla.org/en-US/docs/Web/HTML")    
+html_icon.onclick = () => {
+  window.open("https://developer.mozilla.org/en-US/docs/Web/HTML");
 };
-css_icon.onclick = ()=>{
-    window.open("https://developer.mozilla.org/en-US/docs/Web/CSS")    
+css_icon.onclick = () => {
+  window.open("https://developer.mozilla.org/en-US/docs/Web/CSS");
 };
-js_icon.onclick = ()=>{
-    window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript")    
+js_icon.onclick = () => {
+  window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript");
 };
-js_icon.onclick = ()=>{
-    window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript")    
+js_icon.onclick = () => {
+  window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript");
 };
-bs_icon.onclick = ()=>{
-    window.open("https://getbootstrap.com")
-}
-sass_icon.onclick = ()=>{
-    window.open("https://sass-lang.com")
-}
-react_icon.onclick = ()=>{
-    window.open("https://reactjs.org")
-}
+bs_icon.onclick = () => {
+  window.open("https://getbootstrap.com");
+};
+sass_icon.onclick = () => {
+  window.open("https://sass-lang.com");
+};
+react_icon.onclick = () => {
+  window.open("https://reactjs.org");
+};
 
-alert("if you want to change background double click in any space😄 😄")
+let talk = alert(
+  "if you want to change Name color double click in any space😄 😄"
+);
 
 let curoser = document.querySelector(".curoser");
 let curoser_lazy = document.querySelector(".curoser-lazy");
-let editCuroser = function editCuroser(event){
-    curoser.style.left = event.clientX + 'px';
-    curoser.style.top = event.clientY + 'px';
-    curoser_lazy.style.left = event.clientX + 'px';
-    curoser_lazy.style.top = event.clientY + 'px';
-}
+let editCuroser = function editCuroser(event) {
+  curoser.style.left = event.clientX + "px";
+  curoser.style.top = event.clientY + "px";
+  curoser_lazy.style.left = event.clientX + "px";
+  curoser_lazy.style.top = event.clientY + "px";
+};
 
-window.addEventListener('mousemove', editCuroser)
+window.addEventListener("mousemove", editCuroser);
+let change = document.addEventListener("dblclick", function (event) {
+  let mouseX = event.clientX;
+  let mouseY = event.clientY;
 
+  let red = mouseX % 256;
+  let green = mouseY % 256;
+  let blue = (mouseX + mouseY) % 256;
 
-
-let media = document.querySelector(".container")
-let change = document.addEventListener('dblclick', function(event){
-    let mouseX = event.clientX;
-    let mouseY = event.clientY;
-  
-    let red = mouseX % 256;
-    let green = mouseY % 256;
-    let blue = (mouseX + mouseY) % 256;
-  
-    document.body.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+  document.body.style.color = "rgb(" + red + ", " + green + ", " + blue + ")";
 });
+
+let color = document.querySelector(".color");
+
+color.addEventListener("change", function () {
+  let selectColor = color.value;
+
+  document.body.style.backgroundColor = selectColor;
+
+  if ((document.body.style.color = "rgb(255,255,255)" && "rgb(34,26,153)")) {
+    document.body.style.color = "black";
+  }
+});
+
+let reloadbtn = document.querySelector(".logo");
+
+reloadbtn.onclick = function () {
+window.location.reload();
+};
 
